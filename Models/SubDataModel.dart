47db0b6442b7f798a5204722edf7073a44411ed4
youyoung00 <80229988+youyoung00@users.noreply.github.com>
2021-05-09 01:24:29 +0900

    Create SubDataModel.dart

---
+++ Models/SubDataModel.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+
+class SubDataModel{
+  String title;
+  String singer;
+  String jacket;
+  String lyrics;
+  //List sData;
+
+  SubDataModel({
+    @required this.title,
+    @required this.lyrics,
+    @required this.jacket,
+    @required this.singer,
+    //@required this.sData
+  });
+}
